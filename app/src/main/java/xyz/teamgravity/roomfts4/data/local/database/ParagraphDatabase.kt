575b33f5d34c5ca574192724ab85efd06c3bd332
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-04 15:41:32 +0500

    modules and provide paragraph database

--- app/src/main/java/xyz/teamgravity/roomfts4/data/local/database/ParagraphDatabase.kt
+++ app/src/main/java/xyz/teamgravity/roomfts4/data/local/database/ParagraphDatabase.kt
@@ -1,6 +1,7 @@
 package xyz.teamgravity.roomfts4.data.local.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import xyz.teamgravity.roomfts4.data.local.constant.ParagraphConst
 import xyz.teamgravity.roomfts4.data.local.dao.ParagraphDao
 import xyz.teamgravity.roomfts4.data.local.entity.ParagraphEntity
@@ -10,7 +11,7 @@ import xyz.teamgravity.roomfts4.data.local.fts.ParagraphFtsEntity
     entities = [ParagraphEntity::class, ParagraphFtsEntity::class],
     version = ParagraphConst.VERSION
 )
-abstract class ParagraphDatabase {
+abstract class ParagraphDatabase : RoomDatabase() {
 
     abstract fun paragraphDao(): ParagraphDao
 }
---
+++ app/src/main/java/xyz/teamgravity/roomfts4/injection/provide/ApplicationModule.kt
@@ -0,0 +1,24 @@
+package xyz.teamgravity.roomfts4.injection.provide
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import xyz.teamgravity.roomfts4.data.local.constant.ParagraphConst
+import xyz.teamgravity.roomfts4.data.local.database.ParagraphDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideParagraphDatabase(application: Application): ParagraphDatabase =
+        Room.databaseBuilder(application, ParagraphDatabase::class.java, ParagraphConst.NAME)
+            .createFromAsset("database/paragraphs.db")
+            .fallbackToDestructiveMigration()
+            .build()
+}
