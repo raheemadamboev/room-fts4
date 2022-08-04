4e54e1c3fb96ca457e686abb5668f3e48877b6fe
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-04 15:29:46 +0500

    setup dao

---
+++ app/src/main/java/xyz/teamgravity/roomfts4/data/local/dao/ParagraphDao.kt
@@ -0,0 +1,22 @@
+package xyz.teamgravity.roomfts4.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import xyz.teamgravity.roomfts4.data.local.constant.ParagraphConst.FTS_PARAGRAPH
+import xyz.teamgravity.roomfts4.data.local.constant.ParagraphConst.TABLE_PARAGRAPH
+import xyz.teamgravity.roomfts4.data.local.entity.ParagraphEntity
+
+@Dao
+interface ParagraphDao {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // GET
+    ///////////////////////////////////////////////////////////////////////////
+
+    @Query("SELECT * FROM $TABLE_PARAGRAPH ORDER BY id ASC")
+    fun getParagraphs(): Flow<List<ParagraphEntity>>
+
+    @Query("SELECT SNIPPET($FTS_PARAGRAPH) FROM $TABLE_PARAGRAPH JOIN $FTS_PARAGRAPH ON $TABLE_PARAGRAPH.id == $FTS_PARAGRAPH.rowid WHERE $FTS_PARAGRAPH.paragraph MATCH :query ORDER BY $TABLE_PARAGRAPH.id ASC")
+    fun searchParagraph(query: String): Flow<List<String>>
+}
